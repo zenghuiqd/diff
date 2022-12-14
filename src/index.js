acceb5f2e6b54b1ca7d18da0210dded728f91985
zenghuiqd <1436167699@qq.com>
2022-12-14 16:22:42 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -24,6 +24,14 @@ patch(container, myVnode1);
   h('li',{key:'C'},'C'),
 ]);
 
+const myVnode3 =h("ul", {}, [
+  h('li',{key:'A'},'A'),
+  h('li',{key:'B'},'B'),
+  h('li',{key:'M'},'M'),
+  h('li',{key:'N'},'N'),
+  h('li',{key:'C'},'C'),
+]);
+
  btn.onclick = function(){
   //第二次上树
   patch(myVnode1, myVnode2);
